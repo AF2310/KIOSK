2d227b3a02cc0545ed87669512e3ce4a7a702a62
Michelle Weber <mw224hw@student.lnu.se>
2025-04-25 21:02:00 +0200

    Implemented Arrow Button Class into Main Menu Screen

--- app/src/main/java/org/example/MainMenuScreen.java
+++ app/src/main/java/org/example/MainMenuScreen.java
@@ -146,29 +146,11 @@ public class MainMenuScreen {
     // Arrow buttons to navigate menu
 
     // Arrow left
-    Image leftArrow = new Image(getClass().getResourceAsStream("/nav_bl.png"));
-    ImageView leftArrowView = new ImageView(leftArrow);
-    leftArrowView.setFitHeight(40);
-    leftArrowView.setPreserveRatio(true);
-
-    // Wrap arrow ImageView in a StackPane to make borders of button visible
-    StackPane leftArrowWrapper = new StackPane(leftArrowView);
-    leftArrowWrapper.setStyle(
-        "-fx-border-color: black;"
-        + "-fx-border-width: 3px;"
-        + "-fx-border-radius: 9px;" // For round borders
-        + "-fx-padding: 10px;"      // For wider clickable area
-    );
-    // Setting border size (yes this much is needed)
-    leftArrowWrapper.setMinWidth(60);
-    leftArrowWrapper.setMaxWidth(60);
-    leftArrowWrapper.setPrefWidth(60);
-    leftArrowWrapper.setMinHeight(300);
-    leftArrowWrapper.setMaxHeight(300);
-    leftArrowWrapper.setPrefHeight(300);
+    // Make instance of arrow button that points left
+    ArrowButton leftArrowButton = new ArrowButton(true);
 
     // left button clickable as long as it's still inside set bounds (>0)
-    leftArrowWrapper.setOnMouseClicked(e -> {
+    leftArrowButton.setOnMouseClicked(e -> {
       if (currentCategoryIndex > 0) {
         currentCategoryIndex--;
         updateGrid();
@@ -179,30 +161,11 @@ public class MainMenuScreen {
     });
 
     // Arrow right
-    // Uses same image as left button just mirrowed
-    ImageView rightArrowView = new ImageView(leftArrow);
-    rightArrowView.setFitHeight(40);
-    rightArrowView.setPreserveRatio(true);
-    rightArrowView.setScaleX(-1);
-
-    // Wrap arrow ImageView in a StackPane to make borders of button visible
-    StackPane rightArrowWrapper = new StackPane(rightArrowView);
-    rightArrowWrapper.setStyle(
-          "-fx-border-color: black;"
-          + "-fx-border-width: 3px;"
-          + "-fx-border-radius: 9px;" // For round borders
-          + "-fx-padding: 10px;"      // For wider clickable area
-    );
-    // Setting border size (yes this much is needed)
-    rightArrowWrapper.setMinWidth(60);
-    rightArrowWrapper.setMaxWidth(60);
-    rightArrowWrapper.setPrefWidth(60);
-    rightArrowWrapper.setMinHeight(300);
-    rightArrowWrapper.setMaxHeight(300);
-    rightArrowWrapper.setPrefHeight(300);
+    // Make instance of arrow button that points right
+    ArrowButton rightArrowButton = new ArrowButton(false);
 
     // right button clickable as long as its not in last category (Special offers)
-    rightArrowWrapper.setOnMouseClicked(e -> {
+    rightArrowButton.setOnMouseClicked(e -> {
       if (currentCategoryIndex < categories.length - 1
           && !categories[currentCategoryIndex].equals("Special Offers")) {
         currentCategoryIndex++;
@@ -214,9 +177,9 @@ public class MainMenuScreen {
     });
 
     // Make arrow buttons left right centered vertically
-    VBox leftArrowVcentered = new VBox(leftArrowWrapper);
+    VBox leftArrowVcentered = new VBox(leftArrowButton);
     leftArrowVcentered.setAlignment(Pos.CENTER);
-    VBox rightArrowVcentered = new VBox(rightArrowWrapper);
+    VBox rightArrowVcentered = new VBox(rightArrowButton);
     rightArrowVcentered.setAlignment(Pos.CENTER);
 
     // Add all Menu items and left right buttons in center of menu in the right order
