64b8cc9de2b54e0beb09a364fcad88f6c473a19e
Vuk Vasovic <vv222fw@student.lnu.se>
2025-05-03 16:55:00 +0200

    Worked on the bottom part of the Admin Menu Screen, added the language button and made a similar instance of an already existing cancel button but with text under to follow the design.

--- app/src/main/java/org/example/screens/AdminMenuScreen.java
+++ app/src/main/java/org/example/screens/AdminMenuScreen.java
@@ -1,11 +1,17 @@
 package org.example.screens;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.example.buttons.CancelButtonWithText;
+import org.example.buttons.RoundButton;
 
 /**
  * Admin menu class.
@@ -22,15 +28,37 @@ public class AdminMenuScreen {
       Scene welcomeScrScene) {
     //the mainlayout
     VBox adminMenuLayout = new VBox(20);
-    adminMenuLayout.setAlignment(Pos.CENTER);
+    adminMenuLayout.setAlignment(Pos.TOP_CENTER);
 
     Label adminMenuText = new Label("Admin Menu");
-  
-    adminMenuLayout.getChildren().addAll(adminMenuText);
-    StackPane mainPane = new StackPane(adminMenuLayout);
-    mainPane.setPrefSize(windowWidth, windowHeight);
 
-    Scene adminMenuScene = new Scene(mainPane, windowWidth, windowHeight);
+    adminMenuLayout.getChildren().addAll(adminMenuText);
+    RoundButton langButton = new RoundButton("languages", 70);
+    HBox bottomLeftBox = new HBox(langButton);
+    bottomLeftBox.setAlignment(Pos.BOTTOM_LEFT);
+
+    // Spacer for bottom part of the Screen
+    Region spacerBottom = new Region();
+    HBox.setHgrow(spacerBottom, Priority.ALWAYS);
+
+    CancelButtonWithText cancelButton = new CancelButtonWithText();
+    HBox bottomRightBox = new HBox(cancelButton);
+    bottomRightBox.setAlignment(Pos.BOTTOM_RIGHT);
+
+    cancelButton.setOnAction(e -> {
+      primaryStage.setScene(welcomeScrScene);
+    });
+
+    HBox bottomLayout = new HBox();
+    bottomLayout.setPadding(new Insets(0, 0, 0, 0));
+    bottomLayout.getChildren().addAll(bottomLeftBox, spacerBottom, bottomRightBox);
+    
+    BorderPane mainBorderPane = new BorderPane();
+    mainBorderPane.setPadding(new Insets(50));
+    mainBorderPane.setTop(adminMenuLayout);
+    mainBorderPane.setBottom(bottomLayout);
+
+    Scene adminMenuScene = new Scene(mainBorderPane, windowWidth, windowHeight);
 
     return adminMenuScene;
   }
