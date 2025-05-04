338a281b8d8b12a05cc921a6e07fbacee59e4fda
Vincent <vh222rk@student.lnu.se>
2025-05-04 12:47:14 +0200

    Implemented methods for testing (temporary inserting and then removing an order to the DB)

--- app/src/main/java/org/example/screens/AdminOrdHistoryScreen.java
+++ app/src/main/java/org/example/screens/AdminOrdHistoryScreen.java
@@ -1,15 +1,24 @@
 package org.example.screens;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.example.buttons.BackButton;
-import org.example.sql.SqlConnectionCheck;
+import org.example.buttons.RoundButton;
 
 /**
  * Order History class.
@@ -17,7 +26,9 @@ import org.example.sql.SqlConnectionCheck;
 public class AdminOrdHistoryScreen {
 
   private Stage primaryStage;
-  private Connection connection;
+
+  // for testing purposes
+  private Integer tempOrderId = null;
 
   /**
    * Scene to display the order history.
@@ -30,19 +41,175 @@ public class AdminOrdHistoryScreen {
 
     this.primaryStage = primaryStage;
 
+    // Temporarily inserting an order into db for testing purposes
+    try {
+
+      insertTempOrder();
+
+    } catch (SQLException e) {
+
+      e.printStackTrace();
+      
+    }
+
+    
+    // So the admin doesnt forget where he is lol
+    Label historyLabel = new Label("Order History:");
+    historyLabel.setStyle(
+        "-fx-font-size: 65px;"
+        + "-fx-font-weight: bold;"
+    );
+
+    // Placeholder for the actual history
+    Label tempLabel = new Label("Here will be all the orders");
+    tempLabel.setStyle(
+        "-fx-font-size: 35px;"
+        + "-fx-font-weight: bold;"
+    );
+
+    HBox orderHistory = new HBox(tempLabel);
+      
+    VBox topBox = new VBox();
+    topBox.getChildren().addAll(historyLabel, orderHistory);
+
+    HBox topContainer = new HBox();
+    topContainer.setAlignment(Pos.TOP_LEFT);
 
     // Back button
+    // Clicking button means user goes to previous screen
+    // Also deletes the order that has previously been added to the db
     BackButton backButton = new BackButton();
-    // clicking button means user goes to previous screen
-    backButton.setOnAction(e -> primaryStage.setScene(prevScene));
+    backButton.setOnAction(e -> {
 
-    Region bottomSpacer = new Region()
+      try {
+
+        deleteTempOrder();
+
+      } catch (SQLException ex) {
+
+        ex.printStackTrace();
+
+      }
+
+      primaryStage.setScene(prevScene);
+
+    });
 
 
+    // Language Button
+    // cycles images on click
+    RoundButton langButton = new RoundButton("languages", 70);
+    
+    Region spacerBottom = new Region();
+    HBox.setHgrow(spacerBottom, Priority.ALWAYS);
+    
     HBox bottomContainer = new HBox();
-    bottomContainer.getChildren().addAll(bottomSpacer, backButton);
+    bottomContainer.setAlignment(Pos.BOTTOM_LEFT);
+    bottomContainer.getChildren().addAll(langButton, spacerBottom, backButton);
+
+    // Setting positioning of all the elements
+    BorderPane layout = new BorderPane();
+    layout.setPadding(new Insets(50));
+    layout.setTop(topContainer);
+    layout.setBottom(bottomContainer);
 
     return new Scene(layout, 1920, 1080);
+
+  }
+
+  private ArrayList<Order> queryOrders() throws SQLException {
+
+    ArrayList<Order> history = new ArrayList<>();
+
+    Connection conn = DriverManager.getConnection(
+        "jdbc:mysql://bdzvjxbmj2y2atbkdo4j-mysql.services"
+          + ".clever-cloud.com:3306/bdzvjxbmj2y2atbkdo4j"
+          + "?user=u5urh19mtnnlgmog"
+          + "&password=zPgqf8o6na6pv8j8AX8r"
+          + "&useSSL=true"
+          + "&allowPublicKeyRetrieval=true");
+
+    return history;
+
+  }
+
+  /**
+   * FOR TESTING PURPOSES ONLY!
+   * Inserts a temporary order to the Database.
+   */
+  private void insertTempOrder() throws SQLException {
+
+    String insertSql = 
+        "INSERT INTO `order`(kiosk_ID, customer_ID, amount_total, status) "
+        + "VALUES (?, ?, ?, ?)";
+
+    try (
+        Connection conn = DriverManager.getConnection(
+            "jdbc:mysql://bdzvjxbmj2y2atbkdo4j-mysql.services"
+            + ".clever-cloud.com:3306/bdzvjxbmj2y2atbkdo4j"
+            + "?user=u5urh19mtnnlgmog"
+            + "&password=zPgqf8o6na6pv8j8AX8r"
+            + "&useSSL=true"
+            + "&allowPublicKeyRetrieval=true"
+        );
+
+        PreparedStatement stmt = conn.prepareStatement(
+            insertSql,
+            PreparedStatement.RETURN_GENERATED_KEYS
+        )
+        
+    ) {
+
+      stmt.setInt(1, 1);
+      stmt.setInt(1, 1);
+      stmt.setBigDecimal(3, new java.math.BigDecimal("123"));
+      stmt.setString(4, "PAID");
+
+      stmt.executeUpdate();
+
+      ResultSet rs = stmt.getGeneratedKeys();
+
+      if (rs.next()) {
+
+        tempOrderId = rs.getInt(1);
+
+      }
+
+    }
+
+  }
+
+  /**
+   * FOR TESTING PURPOSES ONLY!
+   * Deletes the temporary order from db.
+   */
+  private void deleteTempOrder() throws SQLException {
+
+    if (tempOrderId == null) {
+
+      return;
+
+    }
+
+    String deleteSql = "DELETE FROM `order` WHERE order_ID = ?";
+
+    try (
+        Connection conn = DriverManager.getConnection(
+            "jdbc:mysql://bdzvjxbmj2y2atbkdo4j-mysql.services"
+            + ".clever-cloud.com:3306/bdzvjxbmj2y2atbkdo4j"
+            + "?user=u5urh19mtnnlgmog"
+            + "&password=zPgqf8o6na6pv8j8AX8r"
+            + "&useSSL=true"
+            + "&allowPublicKeyRetrieval=true"
+        );
+
+        PreparedStatement stmt = conn.prepareStatement(deleteSql)) {
+
+      stmt.setInt(1, tempOrderId);
+      stmt.executeUpdate();
+        
+    }
+
   }
 
 }
