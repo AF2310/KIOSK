c4deda38fd9f14c2d7e54a03257105d45484b408
Michelle Weber <mw224hw@student.lnu.se>
2025-05-05 19:38:47 +0200

    added some commenting

--- app/src/main/java/org/example/users/Customer.java
+++ app/src/main/java/org/example/users/Customer.java
@@ -15,19 +15,26 @@ public class Customer implements User {
   /**
    * Creates a new order.
    *
-   * @throws SQLException error
+   * @throws SQLException SQL Database errors
    */
   public int placeOrder(Connection conn) throws SQLException {
+    // Calculate total order price
     Order order = new Order();
     double price = order.calculatePrice();
 
-    // TODO customer id can be null -> remove/replace with null 
+    // Customer id is NOT nullable for now TODO make nullable
+    // SQL Query as string statement
     String s = "INSERT INTO `order` "
         + "(kiosk_ID, customer_ID, order_date, amount_total, status)"
         + "VALUES (123, 1, NOW(), ?, 'pending')";
     
+    // Prepare statement to be actual query
     PreparedStatement ps = conn.prepareStatement(s);
+
+    // Insert price variable
     ps.setObject(1, price);
+
+    // Execute query
     ps.executeUpdate();
 
     // immediately fetch the auto generated order id
@@ -56,16 +63,24 @@ public class Customer implements User {
    * To not get false IDs on wrong usage, this is private
    * and used in the place order method only.
    *
-   * @throws SQLException error
+   * @throws SQLException SQL Database errors
    */
   private int receiveOrderId(Connection conn) throws SQLException {
+    // Set default order id
     int id = -1;
+
+    // SQL Query as string statement
     String s = "SELECT LAST_INSERT_ID()";
 
+    // Prepare statement to be actual query
+    // Using try to save ressources and close process automatically
     try (PreparedStatement ps = conn.prepareStatement(s)) {
+      // Open result set to fetch order id (execute query)
       ResultSet rs = ps.executeQuery();
 
+      // if there is a result
       if (rs.next()) {
+        // store result as order id integer
         id = rs.getInt(1);
       }
     }
