a4547be777d3979133c27e72f41d8a5e7c1b38be
Vuk Vasovic <vv222fw@student.lnu.se>
2025-05-11 12:04:33 +0200

    connecting to the new database, fixing some checkstyle

--- app/src/main/java/org/example/screens/AdminOrdHistoryScreen.java
+++ app/src/main/java/org/example/screens/AdminOrdHistoryScreen.java
@@ -201,12 +201,12 @@ public class AdminOrdHistoryScreen {
     try (
 
         Connection conn = DriverManager.getConnection(
-            "jdbc:mysql://bdzvjxbmj2y2atbkdo4j-mysql.services"
-            + ".clever-cloud.com:3306/bdzvjxbmj2y2atbkdo4j"
-            + "?user=u5urh19mtnnlgmog"
-            + "&password=zPgqf8o6na6pv8j8AX8r"
-            + "&useSSL=true"
-            + "&allowPublicKeyRetrieval=true"
+          "jdbc:mysql://b8gwixcok22zuqr5tvdd-mysql.services"
+          + ".clever-cloud.com:21363/b8gwixcok22zuqr5tvdd"
+          + "?user=u5urh19mtnnlgmog"
+          + "&password=zPgqf8o6na6pv8j8AX8r"
+          + "&useSSL=true"
+          + "&allowPublicKeyRetrieval=true"
         );
 
         PreparedStatement stmt = conn.prepareStatement(querySql);
--- app/src/main/java/org/example/screens/MealCustomizationScreen.java
+++ app/src/main/java/org/example/screens/MealCustomizationScreen.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
