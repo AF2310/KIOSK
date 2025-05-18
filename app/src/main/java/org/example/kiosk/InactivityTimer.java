9ac15e9d2353f7fe37831f702be8048295a4a5c3
Michelle Weber <mw224hw@student.lnu.se>
2025-05-18 12:32:03 +0200

    Added Inactivity Timer class

---
+++ app/src/main/java/org/example/kiosk/InactivityTimer.java
@@ -0,0 +1,11 @@
+package org.example.kiosk;
+
+/**
+ * This is the timer that starts counting after a
+ * certain amount of inactivity on the terminal.
+ * It will also reset the terminal if nothing is
+ * clicked during an activity check.
+ */
+public class InactivityTimer {
+  
+}
