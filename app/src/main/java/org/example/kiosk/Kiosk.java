642d4c66ccfd1ba0f3dd72790a0d4dcb73df4d3b
Arseny Isaev <ai222tz@student.lnu.se>
2025-05-30 13:02:54 +0200

    By Vince: save name function

--- app/src/main/java/org/example/kiosk/Kiosk.java
+++ app/src/main/java/org/example/kiosk/Kiosk.java
@@ -1,36 +1,36 @@
 package org.example.kiosk;
 
-import com.mysql.cj.x.protobuf.MysqlxCrud.Order;
+// import com.mysql.cj.x.protobuf.MysqlxCrud.Order;
 
 /**
  * Represents a Kiosk terminal.
  */
 public class Kiosk {
 
-  // TODO: These should be changed to private.
-  public String location;
-  public boolean isActive;
+  // // TODO: These should be changed to private.
+  // public String location;
+  // public boolean isActive;
 
-  /**
-   * Displays the menu on the Kiosk screen.
-   */
-  public void displayMenu() {
+  // /**
+  //  * Displays the menu on the Kiosk screen.
+  //  */
+  // public void displayMenu() {
 
-  }
+  // }
 
-  /**
-   * Resets the terminal to its default state.
-   */
-  public void resetTerminal() {
+  // /**
+  //  * Resets the terminal to its default state.
+  //  */
+  // public void resetTerminal() {
 
-  }
+  // }
 
-  /**
-   * Processes the order.
-   *
-   * @param order the order that is processed.
-   */
-  public void processOrder(Order order) {
+  // /**
+  //  * Processes the order.
+  //  *
+  //  * @param order the order that is processed.
+  //  */
+  // public void processOrder(Order order) {
 
-  }
+  // }
 }
